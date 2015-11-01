3190e225b77afbc22eb040cda02c31b209dc64f2
ondrus <josefin.ondrus@gmail.com>
2015-11-01 22:36:03 +0100

    popup works

--- app.js
+++ app.js
@@ -4,22 +4,23 @@
 //Sets the buttons to trigger the blackout on clicks
 $(document).ready(function(){
 
+
+
 	$('#calendar').fullCalendar({
-
-        	//callback
-        dayClick: function() {
-        	
-        	$(".open").click(function () {
-        		$(".popup").fadeIn(500);
-        		$(".cover").fadeTo(500, 0.5);
-    		});
-   		}
-
-	});
-
-	$(".close").click(function () {
-		$(".popup").fadeOut(500);
-        $(".cover").fadeOut(500);
+    	dayClick: function() {
+    //----- OPEN
+    $(this).on('click', function()  {
+        //var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + "popup-1" + '"]').fadeIn(350);
+ 
     });
-
-});
+ 
+    //----- CLOSE
+    $('[data-popup-close]').on('click', function()  {
+        //var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + "popup-1" + '"]').fadeOut(350);
+ 
+    });
+}
+    	});
+	});
