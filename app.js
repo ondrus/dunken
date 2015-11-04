41816a9fce31c50015ae46945c27d3b01b3db4f5
ondrus <josefin.ondrus@gmail.com>
2015-11-04 09:32:02 +0100

    view change ok + some graphical fixes

--- app.js
+++ app.js
@@ -4,23 +4,43 @@
 //Sets the buttons to trigger the blackout on clicks
 $(document).ready(function(){
 
+  var calendar = ""
 
+  $('.button').click(function () {
+    if(this.getAttribute('id')=="day"){
+      $('#calendar').fullCalendar('changeView', "agendaDay");
+    }
+    if(this.getAttribute('id')=="week"){
+      $('#calendar').fullCalendar('changeView', "agendaWeek");
+    }
+    if(this.getAttribute('id')=="month"){
+      $('#calendar').fullCalendar('changeView', "month");
+    }
+  });
 
 	$('#calendar').fullCalendar({
-    	dayClick: function() {
-    //----- OPEN
-    $(this).on('click', function()  {
-        //var targeted_popup_class = jQuery(this).attr('data-popup-open');
-        $('[data-popup="' + "popup-1" + '"]').fadeIn(350);
+    	dayClick: function(date, jsEvent, view) {
+    		//----- OPEN
+    		$(this).on('click', function()  {
+    			
+    			setDateFieldData(date);
+    			//var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        		$('[data-popup="' + "popup-1" + '"]').fadeIn(150);
+    		});
  
+    		//----- CLOSE
+    		$('[data-popup-close]').on('click', function()  {
+      		  //var targeted_popup_class = jQuery(this).attr('data-popup-close');
+       			$('[data-popup="' + "popup-1" + '"]').fadeOut(150);
+ 
+    		});
+		}
     });
- 
-    //----- CLOSE
-    $('[data-popup-close]').on('click', function()  {
-        //var targeted_popup_class = jQuery(this).attr('data-popup-close');
-        $('[data-popup="' + "popup-1" + '"]').fadeOut(350);
- 
-    });
-}
-    	});
-	});
+});
+
+	//get and display the clicked day
+	function setDateFieldData(date){
+    	var dateClicked = String(date.format());
+   		var dateFieldElement = document.getElementById('date_field');
+   		dateFieldElement.textContent = dateClicked;
+	}
